a6846e6d93792217648e53079ca308bd66215df3
Developers@Work <DevelopersWork@users.noreply.github.com>
2022-11-21 10:58:38 +0530

    format code

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ dotenv.config();
 
 import { pythonScriptExecution, Instagram } from './lib/index.js';
 
-const DIR = path.resolve(`${process.env.TEMP_DIRECTORY}`)
+const DIR = path.resolve(`${process.env.TEMP_DIRECTORY}`);
 
 function main() {
-    const instagram = new Instagram(DIR);
-    return (async () => await instagram.main())();
+	const instagram = new Instagram(DIR);
+	return (async () => await instagram.main())();
 }
 
-pythonScriptExecution('./lib/PythonScripts/main.py', main, [process.env.YT_API_KEY, DIR]);
+pythonScriptExecution('./lib/PythonScripts/main.py', main, [
+	process.env.YT_API_KEY,
+	DIR,
+]);
